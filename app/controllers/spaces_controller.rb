b3f34104aa226c3a1d8d2ab607f32a5028bf2397
neutoria lucas <neutorialucas.nl@gmail.com>
2023-11-11 13:44:35 +0200

    edited files

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -9,6 +9,17 @@ class SpacesController < ApplicationController
   end
 
   def new
+    @spaces = Space.new
+  end
 
+  def create
+    @space = Space.new(params[:space])
+    @space.save
+    redirect_to space_path(@space)
+  end
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :location, :size, :amenties, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  #
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "spaces#index"
 
-   get '/spaces/:id', to: 'spaces#show', as: :space
+   get '/spaces/:id', to: 'spaces#show'
 
    get '/spaces/new', to: 'spaces#new'
+
+   post '/spaces', to: 'spaces#create'
+
+   post 'spaces/:id/bookings', to: 'bookings#create'
+
 end
